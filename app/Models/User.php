7acfab108b6900f6f2756d8b08becc62da68cc97
Alex András Mándrik <mndalex@protonmail.com>
2021-07-31 09:36:03 +0200

    Revert "Install laravel ui"
    This reverts commit 417da8a90b7083a1e1729ae5b93cad3faae3d278.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
@@ -17,11 +17,9 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'username',
+        'name',
         'email',
         'password',
-        'first_name',
-        'last_name',
     ];
 
     /**
