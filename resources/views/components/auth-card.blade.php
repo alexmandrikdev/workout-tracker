0eafbaa80b8eac1a3af1b6da888611947eab696b
Alex András Mándrik <mndalex@protonmail.com>
2021-07-31 13:40:46 +0200

    Update auth-card component

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -3,7 +3,15 @@
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full flex sm:max-w-md mt-6 bg-white shadow-md overflow-hidden sm:rounded-t-lg">
+        <a class="px-6 py-4 flex-1 {{ !request()->routeIs('login') ? 'bg-gray-50' : 'font-semibold' }}"
+            href="{{ route('login') }}">Login</a>
+
+        <a class="px-6 py-4 flex-1 {{ !request()->routeIs('register') ? 'bg-gray-50' : 'font-semibold' }}"
+            href="{{ route('register') }}">Register</a>
+    </div>
+
+    <div class="w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-b-lg">
         {{ $slot }}
     </div>
 </div>
